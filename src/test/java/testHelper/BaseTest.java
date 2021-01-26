bb884deacf1743bf0de56dc17ee97785be66d84a
Evgeniy Telepnev <telepnef@yandex.ru>
2021-01-26 20:19:23 +0300

    Added environment

--- src/test/java/testHelper/BaseTest.java
+++ src/test/java/testHelper/BaseTest.java
@@ -4,13 +4,13 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
-    @BeforeEach
-    public void setUp() {
+    @BeforeAll
+    public static void setUp() {
         Configuration.startMaximized = true;
         SelenideLogger.addListener("allure", new AllureSelenide()
                 .savePageSource(true)
